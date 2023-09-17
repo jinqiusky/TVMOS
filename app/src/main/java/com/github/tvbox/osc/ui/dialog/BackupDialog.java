6f7cb7dcd8d72adcbef38bc838300bb54e889c4a
Dwei1120 <110244356+Dwei1120@users.noreply.github.com>
2023-09-17 10:09:30 +0800

    Update BackupDialog.java

--- app/src/main/java/com/github/tvbox/osc/ui/dialog/BackupDialog.java
+++ app/src/main/java/com/github/tvbox/osc/ui/dialog/BackupDialog.java
@@ -62,7 +62,7 @@ public class BackupDialog extends BaseDialog {
         ArrayList<String> result = new ArrayList<>();
         try {
             String root = Environment.getExternalStorageDirectory().getAbsolutePath();
-            File file = new File(root + "/tvbox_backup/");
+            File file = new File(root + "/DweiTV_backup/");
             File[] list = file.listFiles();
             Arrays.sort(list, new Comparator<File>() {
                 @Override
@@ -91,7 +91,7 @@ public class BackupDialog extends BaseDialog {
     void restore(String dir) {
         try {
             String root = Environment.getExternalStorageDirectory().getAbsolutePath();
-            File backup = new File(root + "/tvbox_backup/" + dir);
+            File backup = new File(root + "/DweiTV_backup/" + dir);
             if (backup.exists()) {
                 File db = new File(backup, "sqlite");
                 if (AppDataManager.restore(db)) {
@@ -126,7 +126,7 @@ public class BackupDialog extends BaseDialog {
     void backup() {
         try {
             String root = Environment.getExternalStorageDirectory().getAbsolutePath();
-            File file = new File(root + "/tvbox_backup/");
+            File file = new File(root + "/DweiTV_backup/");
             if (!file.exists())
                 file.mkdirs();
             Date now = new Date();
